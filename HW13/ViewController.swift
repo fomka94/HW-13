e957913abcd3b76578f91187368a8d802e4c2e52
fomka94 <fom-aina@mail.ru>
2022-05-21 22:40:41 +0900

    Initial Commit

---
+++ HW13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW13
+//
+//  Created by Фомина Айина Валерьевна on 21.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
